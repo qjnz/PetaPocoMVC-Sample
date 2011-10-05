b84406ff139f8cd22fe0467a9d96082277838be8
schrodinger <liqiujie2008@gmail.com>
2011-10-05 21:11:29 +1300

    Added the sample asp.net mvc3 project for using the PetaPoco.

---
+++ PetaPocoMVC_Sample/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using PetaPocoMVC_Sample.Models;
+
+namespace PetaPocoMVC_Sample.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //
+        // GET: /Customer/
+
+        public ActionResult Index()
+        {
+            var cm = new CustomerModel();
+            ViewBag.Customers = cm.GetCustomers();
+            return View();
+        }
+
+    }
+}
---
+++ PetaPocoMVC_Sample/Models/CustomerModels.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using NorthwindConnection;
+
+namespace PetaPocoMVC_Sample.Models
+{
+    public class CustomerModel
+    {
+        public IEnumerable<Customer> GetCustomers()
+        {
+            var db = new PetaPoco.Database("NorthwindConnection");
+            return db.Query<Customer>("SELECT * FROM Customers");
+        }
+    }
+}
